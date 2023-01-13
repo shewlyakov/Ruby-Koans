d7c60140b5e34c1a36ca4706d053959258d0ca42
shewlyakov <oleksandrsevlakov@gmail.com>
2023-01-13 12:43:44 +0200

    About triangle project

--- koans/about_triangle_project.rb
+++ koans/about_triangle_project.rb
@@ -1,6 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/neo')
 
-# You need to write the triangle method in the file 'triangle.rb'
 require './triangle'
 
 class AboutTriangleProject < Neo::Koan
